ed3b3766f107a1ab256bcdb52485b2cf0a90580f
Daniel <danielmuratov2003@gmail.com>
2026-04-10 18:58:28 +0600

    chore: add eslint root config and finalize monorepo tooling

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+  ignorePatterns: ['dist/', '.next/', 'node_modules/', '*.js', '!.eslintrc.js'],
+}
